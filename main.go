fd8d74bd819febee65183055cbe09deb65a7ed85
ZShab Niba <44338441+nibazshab@users.noreply.github.com>
2024-02-01 23:41:52 +0800

    Update main.go

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
     "embed"
     "html/template"
     "io"
+    "log"
     "math/rand"
     "net/http"
     "os"
@@ -16,24 +17,64 @@ import (
 
 //go:embed index.html
 var assets embed.FS
+
 const AppData = "tmp"
 
 func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         if r.URL.Path == "/" {
             if r.Method == http.MethodPost {
-                handlePost(w, r)
+                p := handlePost(w, r)
+                if p != "" {
+                    logRecord(r, p)
+                }
             } else {
-                handleWeb(w, r)
+                handleWeb(w)
             }
         } else {
-            handleRaw(w, r)
+            if r.Method == http.MethodGet {
+                handleRaw(w, r)
+            }
         }
     })
     http.ListenAndServe(":10002", nil)
 }
 
-func handleWeb(w http.ResponseWriter, r *http.Request) {
+func handlePost(w http.ResponseWriter, r *http.Request) string {
+    if !strings.HasPrefix(r.Header.Get("Content-Type"), "multipart/form-data") {
+        return ""
+    }
+    if r.ContentLength > 5*1024*1024 {
+        w.Write([]byte("Size " + strconv.FormatInt(r.ContentLength, 10) + "b, too large"))
+        return ""
+    }
+    f, d, _ := r.FormFile("f")
+    defer f.Close()
+    if d == nil {
+        w.Write([]byte("Invalid name (not 'f') or no file"))
+        return ""
+    }
+    t, _ := io.ReadAll(f)
+    if !regexp.MustCompile("^(text|image)/.*").MatchString(http.DetectContentType(t)) {
+        w.Write([]byte("Invalid file type (not Text/Image)"))
+        return ""
+    }
+    p := randStr() + filepath.Ext(d.Filename)
+    os.WriteFile(filepath.Join(AppData, p), t, 0666)
+    w.Write([]byte(r.Host + "/" + p))
+    return p
+}
+
+func logRecord(r *http.Request, p string) {
+    ua := r.Header.Get("user-agent")
+    xff := r.Header.Get("X-Forwarded-For")
+    if xff == "" {
+        xff = r.RemoteAddr
+    }
+    log.Print(xff[:strings.LastIndex(xff, ":")] + " - " + p + " - " + ua)
+}
+
+func handleWeb(w http.ResponseWriter) {
     template.Must(template.ParseFS(assets, "index.html")).Execute(w, nil)
 }
 
@@ -47,30 +88,6 @@ func handleRaw(w http.ResponseWriter, r *http.Request) {
         w.Header().Set("Content-Type", "text/plain; charset=UTF-8")
     }
     w.Write(f)
-}
-
-func handlePost(w http.ResponseWriter, r *http.Request) {
-    if !strings.HasPrefix(r.Header.Get("Content-Type"), "multipart/form-data") {
-        return
-    }
-    if r.ContentLength > 5*1024*1024 {
-        w.Write([]byte("Size " + strconv.FormatInt(r.ContentLength, 10) + "b, too large"))
-        return
-    }
-    f, d, _ := r.FormFile("f")
-    defer f.Close()
-    if d == nil {
-        w.Write([]byte("Invalid name (not 'f') or no file"))
-        return
-    }
-    t, _ := io.ReadAll(f)
-    if !regexp.MustCompile("^(text|image)/.*").MatchString(http.DetectContentType(t)) {
-        w.Write([]byte("Invalid file type (not Text/Image)"))
-        return
-    }
-    p := randStr() + filepath.Ext(d.Filename)
-    os.WriteFile(filepath.Join(AppData, p), t, 0666)
-    w.Write([]byte(r.Host + "/" + p))
 }
 
 func randStr() string {
