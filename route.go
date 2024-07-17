a417640af440a21c2f9240075c023d4aa93dd6a6
ZShab Niba <44338441+nibazshab@users.noreply.github.com>
2024-07-17 15:50:03 +0800

    fix

--- route.go
+++ route.go
@@ -15,6 +15,9 @@ func route(w http.ResponseWriter, r *http.Request) {
             }
         }
     } else {
+        if index == "/favicon.ico" {
+            return
+        } else if index == "/" || index == "/style.css" || index == "/script.js" {
         if index == "/" || index == "/style.css" || index == "/script.js" {
             index_page(w, index)
         } else {
