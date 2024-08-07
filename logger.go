f027ad6bb94935be10fbbe0f5cf87b0a34db50ca
ZShab Niba <44338441+nibazshab@users.noreply.github.com>
2024-08-07 04:17:31 +0000

    v2.4 log.log

--- logger.go
+++ logger.go
@@ -1,15 +1,31 @@
 package main
 
 import (
+    "io"
     "log"
     "net/http"
+    "os"
 )
 
-func logger(r *http.Request, id string) {
+func log_init() {
+    f, err := os.Create(log_)
+    if err != nil {
+        log.Fatalf("log.log error: %v", err)
+    }
+    defer f.Close()
+}
+
+func logging(r *http.Request, id string) {
     ip := r.Header.Get("X-Forwarded-For")
     if ip == "" {
         ip = r.RemoteAddr
     }
+
+    f, _ := os.OpenFile(log_, os.O_APPEND|os.O_RDWR, os.ModePerm)
+    defer f.Close()
+
+    multiWriter := io.MultiWriter(os.Stdout, f)
+    log.SetOutput(multiWriter)
 
     log.Print(id + " - " + ip + " - " + r.Header.Get("user-agent"))
 }
--- route.go
+++ route.go
@@ -2,7 +2,29 @@ package main
 
 import (
     "net/http"
+    "os"
+    "path/filepath"
 )
+
+var (
+    db_  string
+    log_ string
+)
+
+func init() {
+    ex, _ := os.Executable()
+    datadir := filepath.Join(filepath.Dir(ex), "data")
+
+    if _, err := os.Stat(datadir); os.IsNotExist(err) {
+        os.MkdirAll(datadir, os.ModePerm)
+    }
+
+    db_ = filepath.Join(datadir, "pastebin.db")
+    log_ = filepath.Join(datadir, "log.log")
+
+    db_init()
+    log_init()
+}
 
 func route(w http.ResponseWriter, r *http.Request) {
     index := r.URL.Path
@@ -11,7 +33,7 @@ func route(w http.ResponseWriter, r *http.Request) {
         if index == "/" {
             id := upload_data(w, r)
             if id != "" {
-                logger(r, id)
+                logging(r, id)
             }
         }
     } else {
